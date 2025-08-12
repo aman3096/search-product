ac35c0b0e555fb7163e5c3e934b46507863f5ce9
Aman Tandon <aman.tandon3096@gmail.com>
2025-08-12 23:51:43 +0530

    Add: Body parsers

--- search-product/app.js
+++ search-product/app.js
@@ -6,39 +6,24 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 const searchRouter = require('./routes/products');
+const bodyParser = require('body-parser');
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
+app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/api/search', searchRouter);
+app.use('/api', searchRouter);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-app.listen(() =>{
-  console.log(`App started and listening on port ${process.env.PORT || 3000}`);
+app.listen(3001, () =>{
+  console.log(`App started and listening on port ${process.env.PORT || 3001}`);
 })
 
 module.exports = app;
--- search-product/routes/products.js
+++ search-product/routes/products.js
@@ -2,8 +2,17 @@ var express = require('express');
 var router = express.Router();
 const products = require('../controllers/products')
 /* GET users listing. */
-router.post('/', function(req, res, next) {
-  res.send('respond with a resource', products.getProducts(req, res, next));
+router.post('/search', async function(req, res, next) {
+  try {
+    const results = await products.getProducts(req, res, next);
+    res.send(results);
+  } catch (error) {
+    console.error('Error fetching products:', error);
+    res.status(500).json({ message: 'Internal Server Error' });
+  }
 });
 
+router.get('/search', function(req, res) {
+  res.json({ message: 'Search Message' });
+});
 module.exports = router;
