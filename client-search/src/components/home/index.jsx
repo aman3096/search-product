5e17d4ca349750bfc62659e353ba5abe975897b1
Aman Tandon <aman.tandon3096@gmail.com>
2025-08-17 16:09:13 +0530

    add: css features

--- client-search/src/components/home/index.jsx
+++ client-search/src/components/home/index.jsx
@@ -2,7 +2,7 @@
 import React from 'react';
 import { useState } from 'react'
 import { useNavigate } from "react-router-dom";
-
+import './index.css';
 const Home = () => {
     const [product, setProduct] = useState('')
     const navigate = useNavigate();
@@ -15,11 +15,17 @@ const Home = () => {
     return (
         
         <React.Fragment>
-        <h1>Welcome to Styli eCommerce first draft</h1>
+        <h1>Welcome to Styli eCommerce</h1>
             <div className="card">
-                <input placeholder='Enter a product to search' value ={product} onChange={(e)=>{setProduct(e.target.value)}} />
-                <button disabled={!product.length} onClick={navigateToSearchPage}>Search</button>
+                <input 
+                    placeholder='Enter a product to search'
+                    value ={product}
+                    onChange={(e)=>{setProduct(e.target.value)}}
+                    className="search-input"
+                />
             </div>
+            <button className="search-button" disabled={!product.length} onClick={navigateToSearchPage}>Search</button>
+
       </React.Fragment>
     );
 }
--- client-search/src/components/products/index.jsx
+++ client-search/src/components/products/index.jsx
@@ -7,53 +7,57 @@ const Products = () => {
     const [searchParams] = useSearchParams();
     const [products, setProducts] = useState([]);
     const [error, setError] = useState(null);
-    // Get the 'query' parameter from the URL search params
     const query = searchParams.get('query');
+    const [searchTerm, setSearchTerm] = useState(query);
+    // Get the 'query' parameter from the URL search params
     
-    useEffect(() => {
-        const fetchProducts = async () => {
-            // Don't fetch if there's no query term.
-            if (!query) {
-                setProducts([]);
-                return;
-            };
+    const fetchProducts = async () => {
+    // Don't fetch if there's no query term.
+    if (!query) {
+        setProducts([]);
+        return;
+    };
 
-            try {
-                setError(null); // Reset error on new search
-                const result = await axios.post('http://localhost:3002/api/search/', {
-                    location: 'Bangalore',
-                    category: 'Electronics',
-                    searchTerm: query
-                });
-                setProducts(result.data);
-            } catch (err) {
-                setError('Failed to fetch products.');
-                console.error(err);
-            }
-        };
+    try {
+        setError(null); // Reset error on new search
+        const result = await axios.post('http://localhost:3002/api/search/', {
+            location: 'Bangalore',
+            category: 'Electronics',
+            searchTerm: searchTerm || query,
+        });
+        setProducts(result.data);
+    } catch (err) {
+        setError('Failed to fetch products.');
+        console.error(err);
+    }
+};
+    useEffect(() => {
         fetchProducts();
     },[query])
 
   return (
       <div>
-        <h1>Search Results for: "{query || '...'}"</h1>
+        <input value={searchTerm} onChange={(e)=>setSearchTerm(e.target.value)} className="" />
+        <button className="search-button" onClick={() => fetchProducts()}>Search</button>
+        <div>Search Results for: "{searchTerm || '...'}"</div>
         {error && <p className="error">{error}</p>}
-        <p>Found {products.length} products.</p>
 
         <div className="product-list">
             {products.length > 0 && !products.message ? (
-
-                products.map((product) => (
-                    <div key={product?.productid} className="product-item">
-                        <img src={product.productimage} alt={product.productname} className="product-image" />
-                        <h2>{product.productname}</h2>
-                        <p>Category: {product.productcategory}</p>
-                        <p>Price: ${product.rate}</p>
-                    </div>
-                ))
+                <div className="product-grid">
+                    {products.map((product) => (
+                        <div key={product?.productid} className="product-item">
+                            <img src={product.productimage} alt={product.productname} className="product-image" />
+                            <h2>{product.productname}</h2>
+                            <p>Category: {product.productcategory}</p>
+                            <p>Price: ${product.rate}</p>
+                        </div>
+                    ))}
+                </div>
             ) : (
                 <p>No products found.</p>
             )}
+            
         </div>
         </div> 
   );
